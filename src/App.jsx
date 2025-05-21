4ca5af7aa63e587c633b4a397782a2bbbc8f1192
Mahmoud <mahmodlte@gmail.com>
2025-05-21 16:28:32 +0300

    Implemented the routes for home and workers page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Workers from "./pages/Workers";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/workers" element={<Workers />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
