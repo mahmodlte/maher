d422aa49d563f340517349ed97d2feb333639a13
Mahmoud <mahmodlte@gmail.com>
2025-06-04 10:32:51 +0300

    Removed the Header call from the App
    used the Layout as main root componet so the Header and Footer would be displayed on every page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Route, Routes } from "react-router";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Workers from "./pages/Workers";
-
+import Layout from "./components/Layout";
 function App() {
   return (
     <>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/workers" element={<Workers />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/workers" element={<Workers />} />
+        </Route>
       </Routes>
     </>
   );
