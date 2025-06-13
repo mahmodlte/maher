6e9f1ee2e8af44f4efff49ca743ddb86ac8c0715
Mahmoud <mahmodlte@gmail.com>
2025-06-13 09:36:06 +0300

    Invoked how it works section in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../components/HeroSection";
+import HowItWorks from "../components/HowItWorks";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <HowItWorks />
     </div>
   );
 };
